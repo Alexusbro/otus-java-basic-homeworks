22f0fcae4c8193a777aa25de549b7b1ee3e783ea
Alex Ardashov <ardashov.alex@ya.ru>
2026-02-23 11:44:55 +0300

    homework21 completed

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework21/Application.java
@@ -0,0 +1,49 @@
+package ru.otus.java.basic.homeworks.homework21;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        final int ARRAY_SIZE = 200_000_000;
+        double[] doubleArray1 = new double[ARRAY_SIZE];
+        double[] doubleArray2 = new double[ARRAY_SIZE];
+        long startTime1 = System.currentTimeMillis();
+        fillDoubleArray(doubleArray1, 0, ARRAY_SIZE);
+        long endTime1 = System.currentTimeMillis();
+        double result1 = (double) (endTime1 - startTime1) / 1000;
+        System.out.printf("время заполнения массива: %.2f секунд\n", result1);
+        System.out.println("многотопочное заполнение");
+        System.out.println("введите количество потоков:");
+        int amountThread = scan.nextInt();
+        scan.close();
+        int part = ARRAY_SIZE / amountThread;
+        ArrayList<Thread> threads = new ArrayList<>();
+        long startTime2 = System.currentTimeMillis();
+        for (int i = 0; i < amountThread; i++) {
+            int start = i*part;
+            int end = start + part;
+            Thread thread = new Thread(() -> {
+                fillDoubleArray(doubleArray2, start, end);
+            });
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long endTime2 = System.currentTimeMillis();
+        double result2 = (double) (endTime2 - startTime2) / 1000;
+        System.out.printf("%.2f секунд\n", result2);
+    }
+
+    public static void fillDoubleArray(double[] doubleArray, int start, int end) {
+        for (int i = start; i < end; i++) {
+            doubleArray[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+        }
+    }
+
+}
+
