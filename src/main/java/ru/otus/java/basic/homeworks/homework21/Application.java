18e8a2f565792add40370dee69644f20a102da19
Alex Ardashov <ardashov.alex@ya.ru>
2026-02-25 00:27:54 +0300

    пофиксил заполнение массива при дробном делении на части

--- src/main/java/ru/otus/java/basic/homeworks/homework21/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework21/Application.java
@@ -7,24 +7,24 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         Scanner scan = new Scanner(System.in);
-        final int ARRAY_SIZE = 200_000_000;
+        final int ARRAY_SIZE = 100_000_000;
         double[] doubleArray1 = new double[ARRAY_SIZE];
         double[] doubleArray2 = new double[ARRAY_SIZE];
-        long startTime1 = System.currentTimeMillis();
+        long startTime1 = System.nanoTime();
         fillDoubleArray(doubleArray1, 0, ARRAY_SIZE);
-        long endTime1 = System.currentTimeMillis();
-        double result1 = (double) (endTime1 - startTime1) / 1000;
-        System.out.printf("время заполнения массива: %.2f секунд\n", result1);
+        long endTime1 = System.nanoTime();
+        double result1 = (double) (endTime1 - startTime1) / 1_000_000;
+        System.out.printf("время заполнения массива: %.2f милисекунд\n", result1);
         System.out.println("многотопочное заполнение");
         System.out.println("введите количество потоков:");
         int amountThread = scan.nextInt();
         scan.close();
         int part = ARRAY_SIZE / amountThread;
         ArrayList<Thread> threads = new ArrayList<>();
-        long startTime2 = System.currentTimeMillis();
+        long startTime2 = System.nanoTime();
         for (int i = 0; i < amountThread; i++) {
-            int start = i*part;
-            int end = start + part;
+            final int start = i * part;
+            final int end = ((amountThread - i) == 1) ? ARRAY_SIZE : start + part;
             Thread thread = new Thread(() -> {
                 fillDoubleArray(doubleArray2, start, end);
             });
@@ -34,9 +34,9 @@ public class Application {
         for (Thread thread : threads) {
             thread.join();
         }
-        long endTime2 = System.currentTimeMillis();
-        double result2 = (double) (endTime2 - startTime2) / 1000;
-        System.out.printf("%.2f секунд\n", result2);
+        long endTime2 = System.nanoTime();
+        double result2 = (double) (endTime2 - startTime2) / 1_000_000;
+        System.out.printf("время многопоточного заполнения массива:%.2f милисекунд\n", result2);
     }
 
     public static void fillDoubleArray(double[] doubleArray, int start, int end) {
